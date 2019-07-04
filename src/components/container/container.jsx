8ec5f2a39da14b4df21de5de98f64ba580faa896
Subhan Ahmed <subhanahmed047@gmail.com>
2019-07-04 20:32:31 +1000

    removed unused css from container component

--- src/components/container/container.jsx
+++ src/components/container/container.jsx
@@ -4,7 +4,6 @@ const Container = styled.div`
     display: flex;
     justify-content: ${props => props.justify ? props.justify : 'center'};
     flex-wrap: ${props => props.wrap ? props.wrap : 'wrap'};
-    list-style: none;
     margin: 0;
     padding: 0;
 `;
