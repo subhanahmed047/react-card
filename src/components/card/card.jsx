a9ba30b27a25c51d8b471c1c3ef58e41f4ac01ae
Subhan Ahmed <subhanahmed047@gmail.com>
2019-07-04 15:03:01 +1000

    added named imports for card child components.

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import CardWrapper from './wrapper';
-import CardImage from './image';
-import CardContent from './content';
-import CardTitle from './title';
-import CardContentLogo from './logo';
+import { CardWrapper, CardImage, CardContentLogo, CardTitle, CardContent } from '../card';
 
 const Card = ({ card }) => {
     return (
-        <React.Fragment>
-            <CardWrapper>
-                <CardImage src={card.img} alt={card.title} />
-                <CardContent>
-                    <CardContentLogo src={card.logo} alt="Company Logo" />
-                    <CardTitle>{card.title}</CardTitle>
-                </CardContent>
-            </CardWrapper>
-        </React.Fragment >
+        <CardWrapper>
+            <CardImage src={card.img} alt={card.title} />
+            <CardContent>
+                <CardContentLogo src={card.logo} alt="Company Logo" />
+                <CardTitle>{card.title}</CardTitle>
+            </CardContent>
+        </CardWrapper>
     );
 }
 
---
+++ src/components/card/index.js
@@ -0,0 +1,7 @@
+import CardWrapper from './wrapper';
+import CardImage from './image';
+import CardContent from './content';
+import CardTitle from './title';
+import CardContentLogo from './logo';
+
+export { CardWrapper, CardImage, CardContent, CardTitle, CardContentLogo };
